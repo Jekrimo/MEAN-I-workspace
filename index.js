0a5306fa7d51faf2aacf48b4c2e9096eac8d955f
jekrimo <jekrimo@gmail.com>
2016-09-07 18:38:12 -0600

    object

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('hello world!');
+ var person = function objectCon(name,age,course){
+   name: name,
+   age: age,
+   course: course,
+   something: function(){
+     console.log("Blarf!")
+   }
+ }
